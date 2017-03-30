f415da3c2179235c211cf5320c7928c5499d2509
ashwinbhite <ashwin.bhite@gmail.com>
2017-03-30 22:52:53 +0530

    committed by Ashwin

--- app/src/main/java/com/example/ashtech/kilbil/AboutFragment.java
+++ app/src/main/java/com/example/ashtech/kilbil/AboutFragment.java
@@ -18,7 +18,7 @@ public class AboutFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
