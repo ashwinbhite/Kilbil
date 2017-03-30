0f36038bbc4ac12f309a79351c6e6b8ef9684d38
ashwinbhite <ashwin.bhite@gmail.com>
2017-03-30 22:52:25 +0530

    committed by Ashwin

--- app/src/main/java/com/example/ashtech/kilbil/AboutFragment.java
+++ app/src/main/java/com/example/ashtech/kilbil/AboutFragment.java
@@ -23,8 +23,8 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_about, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_about, container, false);
+
 
     }
 
