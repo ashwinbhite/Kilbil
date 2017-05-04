6bfebead29e3ad2e0758bdc46f8a84f86f395523
ashwinbhite <ashwin.bhite@gmail.com>
2017-05-05 00:47:40 +0530

    committed by Ashwin

--- app/src/main/java/com/example/ashtech/kilbil/DiaryActivity.java
+++ app/src/main/java/com/example/ashtech/kilbil/DiaryActivity.java
@@ -45,37 +45,39 @@ public class DiaryActivity extends Activity {
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("School Info.");
+        listDataHeader.add("प्रार्थना");
+
 
         // Adding child data
         List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        top250.add("School Timing");
+        top250.add("Timing for Meeting");
+        top250.add("List of Holidays");
+        top250.add("Diwali Vacation");
+        top250.add("Winter Vacation");
+        top250.add("Summer Vacation");
+        top250.add("Conduct for students");
+        top250.add("Withdrawl Rules");
 
         List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        nowShowing.add("शक्ती दे आम्हास");
+        nowShowing.add("जन गण मन");
+        nowShowing.add("प्रतिज्ञा");
+        nowShowing.add("सरस्वती स्तोत्र");
+        nowShowing.add("हे हंस वाहिनी");
+        nowShowing.add("ॐ असतोमा");
+        nowShowing.add("श्लोक");
+        nowShowing.add("एकात्मता मंत्र");
+        nowShowing.add("नमस्कार माझा या ज्ञानमंदिरा");
+        nowShowing.add("शांति मंत्र");
+        nowShowing.add("पसायदान");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+
+
 
         listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
         listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+
     }
 }
